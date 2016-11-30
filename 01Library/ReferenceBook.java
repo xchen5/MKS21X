ed762b438fd612e4d5518f2cb5dc57fe1b2a9dd8
Xavier Chen <xchen5@stuy.edu>
2016-11-30 06:38:46 -0500

    Ref

---
+++ 01Library/ReferenceBook.java
@@ -0,0 +1,7 @@
+public class ReferenceBook extends LibraryBook {
+    private String collection;
+    public ReferenceBook(String bookTitle, String auth, String number, String call, String col){
+	super(bookTitle, auth, number, call)
+    }
+
+}
