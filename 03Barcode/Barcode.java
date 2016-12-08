98dc5ec6a7322030427e991f16483db284cbafc7
xchen5 <xchen5@stuy.edu>
2016-12-07 20:01:19 -0500

    Finished

--- 03Barcode/Barcode.java
+++ 03Barcode/Barcode.java
@@ -1,4 +1,3 @@
-
 public class Barcode{
 // instance variables
    private String _zip;
@@ -10,7 +9,7 @@ public class Barcode{
 //               or zip contains a non digit
 //               _zip and _checkDigit are initialized.
   public Barcode(String zip) {
-      if (zip.length() != 5){
+      if (zip.length() != 5 && !zip.matches("[0-9]+")){
 	  throw new RuntimeException();
       }
       else {
@@ -21,6 +20,9 @@ public class Barcode{
   public int getDigit(){
       return  _checkDigit;
   }
+  public String getZip(){
+    return _zip;
+  }
 
     private int sumDigits(int num){
 	int sum = 0;
@@ -30,34 +32,35 @@ public class Barcode{
         }
 	return sum;
     }
-	    
+
+
     private String toConvert(String input){
-	if (input.equalsTo("0")){
+	if (input.equals("0")){
 	    return "||:::";
 	}
-	else if (input.equalsTo("1")){
+	else if (input.equals("1")){
 	    return ":::||";
 	}
-	else if (input.equalsTo("2")){
-	    return ":::||";
+	else if (input.equals("2")){
+	    return "::|:|";
 	}
-	else if (input.equalsTo("3")){
-	    return ":::||";
+	else if (input.equals("3")){
+	    return "::||:";
 	}
-	else if (input.equalsTo("4")){
-	    return ":::||";
+	else if (input.equals("4")){
+	    return ":|::|";
 	}
-	else if (input.equalsTo("5")){
-	    return ":::||";
+	else if (input.equals("5")){
+	    return ":|:|:";
 	}
-	else if (input.equalsTo("6")){
-	    return ":::||";
+	else if (input.equals("6")){
+	    return ":||::";
 	}
-	else if (input.equalsTo("7")){
-	    return ":::||";
+	else if (input.equals("7")){
+	    return "|:::|";
 	}
-	else if (input.equalsTo("8")){
-	    return ":::||";
+	else if (input.equals("8")){
+	    return "|::|:";
 	}
 	else {
 	    return "|:|::";
@@ -65,23 +68,45 @@ public class Barcode{
     }
 
 // postcondition: Creates a copy of a bar code.
-//  public Barcode clone(){}
+  public Barcode clone(){
+      Barcode n = new Barcode(_zip);
+      return n;
+  }
 
 
 // postcondition: computes and returns the check sum for _zip
 //  private int checkSum(){}
 
-//postcondition: format zip + check digit + Barcode 
-//ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"      
-//  public String toString(){}
+//postcondition: format zip + check digit + Barcode
+//ex. "084518  |||:::|::|::|::|:|:|::::|||::|:|"
+  public String toString(){
+    String xD = "";
+    for(int i =0; i < _zip.length(); i++) {
+      xD += toConvert(String.valueOf(_zip.charAt(i)));
+    }
+    return "||" + xD + toConvert(Integer.toString(_checkDigit)) + "||";
+  }
 
 
-// postcondition: compares the zip + checkdigit, in numerical order. 
-//  public int compareTo(Barcode other){}
-     
+// postcondition: compares the zip + checkdigit, in numerical order.
+  public int compareTo(Barcode other){
+    int current = Integer.parseInt(_zip + _checkDigit);
+    int another = Integer.parseInt(other.getZip() + other.getDigit());
+    if (current < another) {
+      return -1;
+    }
+    else if (another < current){
+      return 1;
+    }
+    else {
+      return 0;
+    }
+  }
+
   public static void main(String[]args){
       Barcode a  = new Barcode("11547");
       System.out.println(a.getDigit());
+      System.out.println(a.toString());
   }
 
 }
