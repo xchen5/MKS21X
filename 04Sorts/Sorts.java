4d938dfd4b54c48e67ae7ffb99dd3c35619234ca
Xavier Chen <xchen5@stuy.edu>
2016-12-13 06:35:44 -0500

    Added Sorts

---
+++ 04Sorts/Sorts.java
@@ -0,0 +1,30 @@
+import java.util.Arrays;
+public class Sorts{
+  /**EDIT THIS METHOD TO MATCH YOUR NAME 
+  */
+  public static String name(){
+      return "10.Chen.Xavier";
+  }
+
+  /**Selection sort of an int array. 
+  *Upon completion, the elements of the array will be in increasing order.
+  *@param data  the elements to be sorted.
+  */
+  public static void selectionSort(int[] data){
+      for(int i = 0; i < data.length; i++){
+	  for (int n = i + 1; n<data.length; n ++ ){
+	      if (data[n]<data[i]) {
+		  int tempI = data[i];
+		  int tempN = data[n];
+		  data[n] = tempI;
+		  data[i] = tempN;
+	      }
+	  }
+  }
+  }
+    public static void main(String[]args) {
+	int[]a = {11,10,9,3,512};
+	selectionSort(a);
+	System.out.println(Arrays.toString(a));
+    }
+}
